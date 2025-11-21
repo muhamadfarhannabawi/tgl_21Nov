92d0b51f02acd58b3086e76bc56f926c2221a322
muhamadfarhannabawi <1123150170@global.ac.id>
2025-11-21 08:59:31 +0700

    membuat card

---
+++ android/app/src/main/kotlin/com/example/tgl_21/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tgl_21
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tgl_21/pages/latihan_card.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "week 8",
+      theme: ThemeData( primarySwatch: Colors.blue ),
+      home: LatihanCart(),
+    );
+  }
+}
